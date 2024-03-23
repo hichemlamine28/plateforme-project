768187e9eca7464ad13ce5433bcd4b86c26fa41f
hichem <hichemlamine@gmail.com>
2024-03-23 21:36:41 +0100

    Fix full deploy script and fix nagios notification admin

--- script/full_deploy.sh
+++ script/full_deploy.sh
@@ -3,6 +3,7 @@
 cd ../server
 
 vagrant up
+vagrant up pfsense
 
 ansible-playbook -i ../ansible/inventory ../ansible/install_config.yml
 
@@ -14,11 +15,11 @@ ansible-playbook -i ../ansible/inventory ../ansible/install_nas.yml
 
 ansible-playbook -i ../ansible/inventory ../ansible/install_ntp.yml
 
-ansible-playbook -i ../ansible/inventory ../ansible/install_cockpit_webmin.yml
-
 ansible-playbook -i ../ansible/inventory ../ansible/install_snort.yml
 
 ansible-playbook -i ../ansible/inventory ../ansible/install_asterisk_freepbx.yml
 
 ansible-playbook -i ../ansible/inventory ../ansible/install_nagios.yml
+
+ansible-playbook -i ../ansible/inventory ../ansible/install_cockpit_webmin.yml
 
